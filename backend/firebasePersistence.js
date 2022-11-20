1c709b407f4788a0ae28e2794d7edd835ce86933
odzeradodeveloperajava@gmail.com <odzeradodeveloperajava@gmail.com>
2022-11-20 18:52:49 +0100

    adding auth0

--- backend/firebasePersistence.js
+++ backend/firebasePersistence.js
@@ -1,5 +1,5 @@
 const firebaseInit = require('./firebaseInit')
-const { getAuth } = require('firebase/auth')
+const { getAuth, onAuthStateChanged } = require('firebase/auth')
 const { createUser, readUser } = require('./firebaseDatabase')
 
 
@@ -7,14 +7,29 @@ const { createUser, readUser } = require('./firebaseDatabase')
 const auth = getAuth(firebaseInit)
 
 
+//const persistence = async () => {
+//    return (
+//        auth.currentUser !== null ? {
+//            email: auth.currentUser.email,
+//            userName: await readUser(auth.currentUser.email)
+//        } : { email: false}
+//    )
+//}
+
+
 const persistence = async () => {
-    return (
-        auth.currentUser !== null ? {
-            email: auth.currentUser.email,
-            userName: await readUser(auth.currentUser.email)
-        } : { email: false}
-    )
+    const auth = getAuth()
+    const user = auth.currentUser;
+    if(user){
+        return user
+    }
+    else{
+        return (' ni ma')
+    }
+   
 }
+
+
 
 module.exports = {
     persistence
--- index.js
+++ index.js
@@ -11,45 +11,51 @@ const loginUser = require('./backend/firebaseLogin');
 const logOutUser = require('./backend/firebaseLogOut');
 const persistence = require('./backend/firebasePersistence');
 const picDownloader =  require('./backend/picDownloader');
+//const auth0Config =  require('./backend/auth0-login')
+const { auth } = require('express-openid-connect');
+const auth0Config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.ACCESS_TOKEN_SECRET,
+    baseURL: 'http://localhost:9000',
+    clientID: 'yXaUDltnI0duAYin15e2qi0XHiKfg1SN',
+    issuerBaseURL: 'https://dev-w17xor5glatrv1zy.us.auth0.com'
+    };
+app.use(auth(auth0Config));
+const { requiresAuth } = require('express-openid-connect');
+
 const jwt = require('jsonwebtoken');
 app.use(express.json());
 app.use(cors());
 
 
-
-const catPicsApi = () => app.get('/', async(req,res) =>{
+app.get('/', async(req,res) =>{
     const pictureLink = 'https://cataas.com/cat';
     res.send(`<img src="${await picDownloader.picDownloader(pictureLink)}"/>`)
 })
 
-const dogPicsApi = () => app.get('/dog', async(req,res) =>{
+app.get('/dog', async(req,res) =>{
     const raw = await axios.get('https://dog.ceo/api/breeds/image/random');
     const pictureLink = raw.data.message
     res.send(`<img src="${await picDownloader.picDownloader(pictureLink)}"/>`)
 })
 
-
-const ccreateUser = () => app.post('/createuser',  async(req,res) =>{
+app.post('/createuser',  async(req,res) =>{
     console.log(req.body.password, req.body.email)
     res.send(await createUser.createUser(req.body.email, req.body.password))
 })
 
-
-const lloginUser = () => app.post('/loginuser',  async(req,res) =>{
-    
+app.post('/loginuser',  async(req,res) =>{ 
     const user = req.body.email
     console.log(user)
     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET )
     res.send(await loginUser.loginUser(req.body.email, req.body.password, accessToken))
 })
 
-
-
-
-const llogOut = () => app.get('/logout',  async(req,res) =>{
+app.get('/logout',  async(req,res) =>{
     res.send(await logOutUser.logOutUser(req.body.email, req.body.password))
 })
-const hpersistence = () => app.get('/persistence',  async(req,res) =>{
+app.get('/persistence',  async(req,res) =>{
     res.send(await persistence.persistence())
 })
 
@@ -57,6 +63,13 @@ app.get('/authtoken', authenticateToken , (req,res) =>{
     res.status(200).send('zalogowano')
     console.log(req.user)
 })
+app.get('/auth0', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? res.redirect('/profile') : res.redirect('/login'));
+});
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user.email));
+  });
+
 
 
 function authenticateToken(req, res, next) {
@@ -70,17 +83,6 @@ function authenticateToken(req, res, next) {
       next()
     })
   }
-
-
-
-llogOut()
-dogPicsApi()
-catPicsApi()
-ccreateUser()
-lloginUser()
-hpersistence()
-
-
 
 
 
