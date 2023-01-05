de5dab66894faa82518c166e3bbc1c10f934200e
Sławomir Tuszkowski <slawomirtuszkowski@Sawomirs-MacBook-Air.local>
2023-01-05 12:42:28 +0100

    fixing log in response

--- index.js
+++ index.js
@@ -103,6 +103,10 @@ app.post('/register', async (req, res)=>{
 })
 
 app.post('/login', function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+res.header('Access-Control-Allow-Origin', req.headers.origin);
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     passport.authenticate('local', function(err, user, info) {
     res.send(info)
     })(req, res, next);
