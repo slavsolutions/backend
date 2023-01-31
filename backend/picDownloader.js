748c6793bbfe32230039b47c4d8b191e3f569e17
Sławomir Tuszkowski <slawomirtuszkowski@ee:41:18:06:e5:52.kpn>
2023-01-31 19:04:05 +0100

    adding error handler to prevent app crash

--- backend/picDownloader.js
+++ backend/picDownloader.js
@@ -3,12 +3,16 @@ const {Buffer} = require('buffer');
 
 
 const picDownloader = async (apiLink) =>{
-    const apiPic = await axios.get(apiLink,{
-        responseType: 'arrayBuffer',
-        responseEncoding: 'binary'
-    })
-    let base64 = Buffer.from(apiPic.data, "binary").toString("base64");
-    let image = `data:${apiPic.headers["content-type"]};base64,${base64}`;
-    return image
+    try{
+        const apiPic = await axios.get(apiLink,{
+            responseType: 'arrayBuffer',
+            responseEncoding: 'binary'
+        })
+        let base64 = Buffer.from(apiPic.data, "binary").toString("base64");
+        let image = `data:${apiPic.headers["content-type"]};base64,${base64}`;
+        return image
+    } catch(e) {
+        console.log(e)
+    }
 }
 module.exports = { picDownloader }
